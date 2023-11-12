6d9c06dc56c7c0db94ec3c88564cde922e2a3367
Goggles <vjedi729@gmail.com>
2023-11-12 11:20:15 -0600

    Split "standard" components from "app-specific" components.

--- budget-planner/src/app/transactions/page.tsx
+++ budget-planner/src/app/transactions/page.tsx
@@ -6,7 +6,7 @@ import { Purchase } from "@/data/purchase"
 
 import { getBucketBalances, testTransactionData } from "@/data/testData"
 import { BucketName } from "@/data/enums"
-import { CumulativeTableColumn, SimpleColumn } from "@/components/react-data-table-component-utils"
+import { CumulativeTableColumn, SimpleColumn } from "@/standard-components/react-data-table-component-utils"
 
 function dollarFormat(n: number): string { return `${n<0 ? "-" : ""}$${Math.round(Math.abs(n)*100)/100}` }
 
