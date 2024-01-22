f47275ec6fc5d2b1af3543212ce8ab9f4aa3c63b
meridith-e <meridith.embry@gmail.com>
2024-01-22 15:43:31 -0600

    made receipt object and deleted unusable reccuring transaction code in comments

--- budget-planner/src/data/receipt.ts
+++ budget-planner/src/data/receipt.ts
@@ -1,9 +1,36 @@
-import { ExternalTransaction } from "./transactions";
+import { Account } from "./account"
+import { Purchase } from "./purchase"
+import { Vendor } from "./vendor"
 
 
-export class receipt{
-    public transaction = ExternalTransaction;
-    public itemCount = number;
+export class Receipt<TimeType = Date> {
+    public time: TimeType;
+    public amount: number;
+    //public tax: ;
+    public vendor: Vendor;
+    public account: Account;
+    public purchases: Purchase[];
+    public itemCount?: number;
+
+
+
     
+    constructor(time: TimeType, amount: number, vendor: Vendor, account: Account, purchases: Purchase[] = [], itemCount: number){
+        this.time = time;
+        this.amount = amount;
+        this.vendor = vendor;
+        this.account = account;
+        this.purchases = purchases;
+        this.itemCount = itemCount;
+    }
 
+    unaccountedItems() {
+        if (this.itemCount != undefined){
+        var remainingItems = this.itemCount - this.purchases.length;
+            if(remainingItems > 0) {
+            console.log(remainingItems.toString() + "items unaccounted for")
+            }
+        }
+    }
+    
 }
